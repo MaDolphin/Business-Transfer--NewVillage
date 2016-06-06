a148e243da98f08d4d45bd255c5ca4ceebc8ae63
MaDolphin <717119hq7@sina.com>
2016-06-06 10:15:23 +0800

    by hhk

---
+++ src/com/NewVillage/interceptor/InvestigatorInterceptor.java
@@ -0,0 +1,47 @@
+package com.NewVillage.interceptor;
+
+import com.NewVillage.entity.Employee;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+import java.util.Map;
+
+public class InvestigatorInterceptor implements Interceptor {
+
+    private Employee employee;
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
+        // TODO Auto-generated method stub
+        Map<String, Object> session= invocation.getInvocationContext().getSession();
+        if(session.get("employee")!=null){
+            Employee employee = (Employee) session.get("employee");
+            if("勘查员".equals(employee.getEmpDep())){
+                return invocation.invoke();
+            }
+            else{
+                return "login";
+            }
+        }else{
+            return "login";
+        }
+    }
+}
